57cbcc20690348cf4c9112009c5e59a567467dec
boyce01 <boycezhao64@gmail.com>
2015-10-05 16:08:23 +1300

    MD5 encode

--- BLL/Login.cs
+++ BLL/Login.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Web.Security; //用于MD5转换
+
 namespace BLL
 {
     /// <summary>
@@ -22,6 +24,12 @@ namespace BLL
         {
             DAL.Login login = new DAL.Login();
             Model.Student model = login.GetName(name);
+
+            //把用户输入的pwd进行MD5转换，再与数据库得到的pwd(MD5加密后的)对比
+            //1）添加引用；2）添加namespace:using System.Web.Security;
+            pwd = FormsAuthentication.HashPasswordForStoringInConfigFile(pwd, "md5").ToLower();
+            //微软提供的FormsAuthentication类的，HashPasswordForStoringInConfigFile()方法，会把加密后的MD5字符串自动
+            //转成大写，故：要ToLower()
 
             if (model == null)
             {
--- BLL/Register.cs
+++ BLL/Register.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Web.Security; //用于MD5
+
 namespace BLL //添加了对DAL类库的引用，以便调用DAL类库中的类
 {
     /// <summary>
@@ -20,6 +22,11 @@ namespace BLL //添加了对DAL类库的引用，以便调用DAL类库中的类
         public bool RegisterAccount(string name, string pwd)
         {
             DAL.Register register = new DAL.Register();
+            //把用户输入的密码，进行MD5加密，再存入数据库
+            pwd = FormsAuthentication.HashPasswordForStoringInConfigFile(pwd, "md5").ToLower();
+            //微软提供的FormsAuthentication类的，HashPasswordForStoringInConfigFile()方法，会把加密后的MD5字符串自动
+            //转成大写，故：要ToLower()
+
             int res = register.RegisterAccount(name, pwd);
             if (res==1)
             {
